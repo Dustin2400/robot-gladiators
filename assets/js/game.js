215623af76ed9568ecfff8f799ed4eebc2c3ba26
Dustin Grijalva <dustin2400@hotmail.com>
2021-09-25 01:53:20 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,13 +59,40 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i=0; i < enemyNames.length; i++) {
-    if (playerHealth > 0){
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
-    }else{
-        window.alert("You have lost your robot in battle! Game Over!");
+var startGame = function() {
+
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i=0; i < enemyNames.length; i++) {
+        if (playerHealth > 0){
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }else{
+            window.alert("You have lost your robot in battle! Game Over!");
+        }
     }
+    endGame();
 }
+
+var endGame = function() {
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You have a score of " + playerMoney + " .");
+    } else {
+        window.alert("You've lost your robot in battle.");
+    }
+    window.alert("The game has ended. Let's see how you did.");
+    var playAgainConfirm = window.confirm("Would you like to play again?")
+
+    if (playAgainConfirm) {
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+
+}
+
+startGame();
